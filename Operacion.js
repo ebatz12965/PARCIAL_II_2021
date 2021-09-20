498540dbfc1e8049c7e1ec7c56763fac436f10f6
Edwing Batz <ebatzm@miumg.edu.gt>
2021-09-19 20:32:11 -0600

    Se generaron los archivos Display.js para la captura de datos, operacion.js para las operaciones (hasta ahora básicas), index.js para los constructores

---
+++ Operacion.js
@@ -0,0 +1,18 @@
+class Operacion{
+    basic_operar(num1, num2, operador){
+        this.solucion = (num1+operador+num2);
+        this.numPantalla = eval(this.solucion);
+        return this.numPantalla;
+    }
+    advanced_operar(num1, num2, operador){
+        this.num1 = parseFloat(num1);
+        this.num2 = parseFloat(num2);
+
+        switch (operador){
+            case 'raiz':
+                this.numPantalla = Math.sqrt(this.num2, 1/this.num1);
+                return this.numPantalla;
+                break;
+        }
+    }
+}
